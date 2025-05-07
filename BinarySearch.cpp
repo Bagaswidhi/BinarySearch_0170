3ce2c60084f0c8ae14ad17936e97c5b2fd52115e
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-05-07 15:01:55 +0700

    Memanggil library dan mendeklarasikan panjang array, variable n, dan variable x

---
+++ BinarySearch.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[10]; // Deklarasi Variable Global array dengan panjang 20
+int n;       // Deklarasi Variable Global n untuk menyimpan elemen pada array
+int x;      // Deklarasi Variable global x untuk menyimpan elemen yang akan dicari pada array
+
