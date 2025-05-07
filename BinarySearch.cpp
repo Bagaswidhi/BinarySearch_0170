7608ff9d4ad9baa31113de9c4caae96c2e27785e
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-05-07 15:05:54 +0700

    Menambahkan prosedur input data

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -22,4 +22,15 @@ void input() // Procedure input
             cout << "Array maksimal adalah 10" << endl;
         }
     }
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "Masukkan elemen pada array" << endl;
+    cout << "==========================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> arr[i];
+    }
 }
+    
