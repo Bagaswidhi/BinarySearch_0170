5fcb6149b774a84d1a55671e2707377b73ee1ab6
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-05-07 15:04:29 +0700

    Mengatur maksimal panjang array tidak melebihi 10 elemen

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -6,3 +6,20 @@ int arr[10]; // Deklarasi Variable Global array dengan panjang 20
 int n;       // Deklarasi Variable Global n untuk menyimpan elemen pada array
 int x;      // Deklarasi Variable global x untuk menyimpan elemen yang akan dicari pada array
 
+void input() // Procedure input
+{
+    int d;
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Array maksimal adalah 10" << endl;
+        }
+    }
+}
